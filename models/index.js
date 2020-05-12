bf67cbaa03d4704a541c37c06aece787a9986199
Johan Nordström <johan@digitalvillage.se>
2020-05-12 11:05:43 +0200

    Result of "npx sequelize init".

--- models/index.js
+++ models/index.js
@@ -1,39 +1,37 @@
+'use strict';
+
+const fs = require('fs');
+const path = require('path');
 const Sequelize = require('sequelize');
+const basename = path.basename(__filename);
+const env = process.env.NODE_ENV || 'development';
+const config = require(__dirname + '/../config/config.json')[env];
+const db = {};
 
-const sequelize = new Sequelize(
-	process.env.DB_NAME,
-	process.env.DB_USER,
-	process.env.DB_PASSWORD,
-	{
-		host: process.env.DB_HOST,
-		port: process.env.DB_PORT || 3306,
-		dialect: process.env.DB_TYPE,
-	}
-);
-
-class Author extends Sequelize.Model {}
-Author.init({
-	first_name: Sequelize.STRING,
-	last_name: Sequelize.STRING,
-	birthyear: Sequelize.INTEGER,
-}, {
-	sequelize,
-	modelName: 'authors',
-});
-
-class Book extends Sequelize.Model {}
-Book.init({
-	title: Sequelize.STRING,
-	isbn: Sequelize.STRING,
-	pages: Sequelize.INTEGER,
-	author_id: Sequelize.INTEGER,
-}, {
-	sequelize,
-	modelName: 'books',
-});
-
-module.exports = {
-	sequelize,
-	Author,
-	Book,
+let sequelize;
+if (config.use_env_variable) {
+  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+} else {
+  sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
+
+fs
+  .readdirSync(__dirname)
+  .filter(file => {
+    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+  })
+  .forEach(file => {
+    const model = sequelize['import'](path.join(__dirname, file));
+    db[model.name] = model;
+  });
+
+Object.keys(db).forEach(modelName => {
+  if (db[modelName].associate) {
+    db[modelName].associate(db);
+  }
+});
+
+db.sequelize = sequelize;
+db.Sequelize = Sequelize;
+
+module.exports = db;
