8600c62b6aed88cd0e9084bbceab872ec6cf5b66
Johan Nordström <johan@digitalvillage.se>
2020-05-12 13:45:35 +0200

    Added bookshelf initialisation and model definitions.

--- models/index.js
+++ models/index.js
@@ -1,1 +1,27 @@
-module.exports = {}
+// Setting up the database connection
+const knex = require('knex')({
+	client: 'mysql',
+	connection: {
+		host: process.env.DB_HOST || 'localhost',
+		port: process.env.DB_PORT || 3306,
+		user: process.env.DB_USER || 'library',
+		password: process.env.DB_PASSWORD || '',
+		database: process.env.DB_NAME || 'library',
+	}
+});
+
+const bookshelf = require('bookshelf')(knex);
+
+const Author = bookshelf.model('Author', {
+	tableName: 'authors',
+});
+
+const Book = bookshelf.model('Book', {
+	tableName: 'books',
+});
+
+module.exports = {
+	bookshelf,
+	Author,
+	Book,
+};
