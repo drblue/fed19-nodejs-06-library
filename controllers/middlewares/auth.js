12f8b2eb0d86eb247f0d6627c3aa9a89a49af069
Johan Nordström <johan@digitalvillage.se>
2020-05-14 16:15:19 +0200

    Added HTTP Basic Authentication to our app.

---
+++ controllers/middlewares/auth.js
@@ -0,0 +1,54 @@
+/**
+ * Authentication middleware
+ */
+
+const { User } = require('../../models');
+
+const basic = async (req, res, next) => {
+	console.log("Hello from auth.basic!");
+
+	// check if Authorization header exists, otherwise bail
+	if (!req.headers.authorization) {
+		res.status(401).send({
+			status: 'fail',
+			data: 'Authorization required',
+		});
+		return;
+	}
+
+	// "Basic a2FsbGUyMDAwOnNjcmlwdC1raWRxd2Vxd2Vxd2Vxd2Vxd2Vxd2Vxd2U="
+	// =>
+	// [0] = "Basic"
+	// [1] = "a2FsbGUyMDAwOnNjcmlwdC1raWRxd2Vxd2Vxd2Vxd2Vxd2Vxd2Vxd2U="
+	const [authSchema, base64Payload] = req.headers.authorization.split(' ');
+
+	if (!authSchema.toLowerCase() === "basic") {
+		// not ours to authenticate
+		next();
+	}
+
+	const decodedPayload = new Buffer(base64Payload, 'base64').toString('ascii');
+
+	// kalle:omg-food
+	const [username, password] = decodedPayload.split(':');
+
+	// ask db if it has a matching user with the same username and password
+	const user = await new User({ username, password }).fetch({ require: false });
+	if (!user) {
+		res.status(401).send({
+			status: 'fail',
+			data: 'Authorization failed',
+		});
+		return;
+	}
+
+	// now that we have authenticated the user and know that he/she/it is who it claims to be
+	// attach the user object to the request, so that other parts of the api can use the user
+	req.user = user;
+
+	next();
+}
+
+module.exports = {
+	basic,
+}
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../controllers/middlewares/auth');
 
 /* GET / */
 router.get('/', (req, res) => {
 	res.send({ status: 'you had me at EHLO' });
 });
+
+// Protect all routes below with the middleware `basic` from the `auth` module.
+router.use(auth.basic);
 
 router.use('/authors', require('./authors'));
 router.use('/books', require('./books'));
