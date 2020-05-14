121902572788a2af226a1e3f7ba9601ae0482399
Johan Nordström <johan@digitalvillage.se>
2020-05-14 12:07:51 +0200

    Added user update functionality (with validation! 😎).

--- validation_rules/user.js
+++ validation_rules/user.js
@@ -19,7 +19,11 @@ const createRules = [
 	body('last_name').isLength({ min: 2 }),
 ];
 
-const updateRules = [];
+const updateRules = [
+	body('password').optional().isLength({ min: 3 }),
+	body('first_name').optional().isLength({ min: 2 }),
+	body('last_name').optional().isLength({ min: 2 }),
+];
 
 module.exports = {
 	createRules,
