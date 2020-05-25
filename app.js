f503ed398fc3ce899ee96950f6d8304d9be76082
Johan Nordström <johan@digitalvillage.se>
2020-05-25 12:19:34 +0200

    Added support for CORS.

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const logger = require('morgan');
 
 const app = express();
 
 app.use(logger('dev'));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
